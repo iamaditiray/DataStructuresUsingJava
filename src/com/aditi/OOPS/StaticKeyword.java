de6569e8b52b5d03f16d272a92fd33bed80e05fa
Aditi Ray <imakeothersmile@gmail.com>
2023-07-02 12:55:43 +0530

    Static Keyword

--- src/com/aditi/OOPS/StaticKeyword.java
+++ src/com/aditi/OOPS/StaticKeyword.java
@@ -1,4 +1,14 @@
 package com.aditi.OOPS;
-
+class Students{
+    static String name;
+    int RollNumber;
+}
 public class StaticKeyword {
+    public static void main(String[] args) {
+        Students.name="Aditi Ray"; //Static Variable can be directly accessed without creating objects
+        Students s1=new Students();
+        s1.RollNumber=20;
+        System.out.println(s1.RollNumber);
+        System.out.println(s1.name);
+    }
 }
